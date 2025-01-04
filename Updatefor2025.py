d21232df317ce49512e1e79824bebf749095933f
Kovalski Pingvin <golubgolubovic@yandex.com>
2025-01-04 20:27:43 +0100

    Create Updatefor2025.py

---
+++ Updatefor2025.py
@@ -0,0 +1,5 @@
+Hello,
+just some things about this project
+
+1. @Golub-LLC cooinblockchain does is not valid Golub-LLC org is a valid contributor but its not valid.
+2. The exiting release of cooin 2.0 is happening. it is in Golub-LLC so wait until they realease it
